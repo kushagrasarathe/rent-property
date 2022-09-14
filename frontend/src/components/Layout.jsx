f8fb03c736496f0681d9581ae334c1148ded5b99
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-09-14 21:35:58 +0530

    added layout component

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <nav>
+        <li></li>
+        <li></li>
+        <li></li>
+      </nav>
+      {children}
+    </>
+  );
+}
