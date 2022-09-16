198092a7e87b7b18534887f0d5d30dbac5857b50
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-09-16 15:58:35 +0530

    name filter working

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,32 +3,107 @@ import Image from "next/image";
 import Card from "../src/components/Card";
 import Filter from "../src/components/Filter";
 import styles from "../styles/Home.module.css";
+import styles2 from "../src/css/Component.module.css";
+import css from "../styles/Global.module.css";
 import { baseURL, fetchData } from "../src/utils/fetchAPI";
 import { useState } from "react";
 import SearchFilters from "../src/components/SearchFilters";
 
 export default function Home({ propertiesForRent }) {
+  const [name, setName] = useState("");
+  const [size, setSize] = useState();
+  const [amount, setAmount] = useState();
   // console.log(propertiesForRent)
+  // console.log(name);
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <meta
-          name="description"
-          content="Rent a prooperty app built with Nextjs"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <div className={styles.container}>
+        <Head>
+          <title>Create Next App</title>
+          <meta
+            name="description"
+            content="Rent a prooperty app built with Nextjs"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <Filter />
-        <div className={styles.listing}>
-          {propertiesForRent.map((property) => (
-            <Card key={property.id} property={property} />
-          ))}
-        </div>
-      </main>
-    </div>
+        <main className={styles.main}>
+          {/* <Filter /> */}
+
+          <div className={styles2.filter}>
+            <div className={styles2.section}>
+              <span>Name</span>
+              <input
+                onChange={(e) => setName(e.target.value)}
+                type="text"
+                name=""
+                id=""
+              />
+            </div>
+            <span className={styles2.vr}></span>
+            <div className={styles2.section}>
+              <span>
+                Size
+                <small>
+                  &#40; in m<sup>2</sup>&#41;
+                </small>{" "}
+              </span>
+              <input
+                onChange={(e) => setSize(e.target.value)}
+                type="number"
+                name=""
+                id=""
+              />
+            </div>
+            <span className={styles2.vr}></span>
+            <div className={styles2.section}>
+              <span>
+                Price
+                <small>&#40; in AED &#41;</small>{" "}
+              </span>
+              <input
+                onChange={(e) => setAmount(e.target.value)}
+                type="number"
+                name=""
+                id=""
+              />
+            </div>
+            <span className={styles2.vr}></span>
+            <div className={styles2.section}>
+              <button className={css.btn_primary}>Search</button>
+            </div>
+          </div>
+
+          {propertiesForRent &&
+            propertiesForRent.map((property) =>
+              // console.log(property.title.toLowerCase())
+              property.title.toLowerCase().includes(name) ? (
+                <div className={styles.listing}>
+                  <Card key={property.id} property={property} />
+                </div>
+              ) : (
+                ''
+              )
+            )}
+
+          {/* { propertiesForRent.price <= price ? (
+             <div className={styles.listing}>
+             {propertiesForRent.map((property) => (
+               <Card key={property.id} property={property} />
+             ))}
+           </div>
+          ) : (
+            <h2>No data found</h2>
+          )} */}
+
+          {/* <div className={styles.listing}>
+            {propertiesForRent.map((property) => (
+              <Card key={property.id} property={property} />
+            ))}
+          </div> */}
+        </main>
+      </div>
+    </>
   );
 }
 
--- frontend/src/components/Filter.jsx
+++ frontend/src/components/Filter.jsx
@@ -17,8 +17,11 @@ export default function Filter() {
       <div className={styles.section}>
         <span>Name</span>
         <input
-        onChange={(e) => setName(e.target.value)}
-        type="text" name="" id="" />
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          name=""
+          id=""
+        />
       </div>
       <span className={styles.vr}></span>
       <div className={styles.section}>
@@ -29,31 +32,25 @@ export default function Filter() {
           </small>{" "}
         </span>
         <input
-        onChange={(e) => setSize(e.target.value)}
-        type="number" name="" id="" />
+          onChange={(e) => setSize(e.target.value)}
+          type="number"
+          name=""
+          id=""
+        />
       </div>
       <span className={styles.vr}></span>
       <div className={styles.section}>
-      <span>
+        <span>
           Price
-          <small>
-            &#40; in AED &#41;
-          </small>{" "}
+          <small>&#40; in AED &#41;</small>{" "}
         </span>
         <input
-        onChange={(e) => setPrice(e.target.value)}
-        type="number" name="" id="" />
+          onChange={(e) => setPrice(e.target.value)}
+          type="number"
+          name=""
+          id=""
+        />
       </div>
-      {/* <span className={styles.vr}></span>
-      <div className={styles.section}>
-        <span>Property Type</span>
-        <select defaultValue="house" onChange={(e) => setType(e.target.value)}>
-          <option value="house">Houses</option>
-          <option value="villa">Villas</option>
-          <option value="apartment">Apartments</option>
-          <option value="hostel">Hostels</option>
-        </select>
-      </div> */}
       <span className={styles.vr}></span>
       <div className={styles.section}>
         <button className={css.btn_primary}>Search</button>
@@ -62,25 +59,25 @@ export default function Filter() {
   );
 }
 
-export async function getServerSideProps({ query }) {
-  const purpose = query.purpose || "for-rent";
-  const rentFrequency = query.rentFrequency || "yearly";
-  const minPrice = query.minPrice || "0";
-  const maxPrice = query.maxPrice || "1000000";
-  const roomsMin = query.roomsMin || "0";
-  const bathsMin = query.bathsMin || "0";
-  const sort = query.sort || "price-desc";
-  const areaMax = query.areaMax || "35000";
-  const locationExternalIDs = query.locationExternalIDs || "5002";
-  const categoryExternalID = query.categoryExternalID || "4";
+// export async function getServerSideProps({ query }) {
+//   const purpose = query.purpose || "for-rent";
+//   const rentFrequency = query.rentFrequency || "yearly";
+//   const minPrice = query.minPrice || "0";
+//   const maxPrice = query.maxPrice || "1000000";
+//   const roomsMin = query.roomsMin || "0";
+//   const bathsMin = query.bathsMin || "0";
+//   const sort = query.sort || "price-desc";
+//   const areaMax = query.areaMax || "35000";
+//   const locationExternalIDs = query.locationExternalIDs || "5002";
+//   const categoryExternalID = query.categoryExternalID || "4";
 
-  const data = await fetchData(
-    `${baseURL}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`
-  );
+//   const data = await fetchData(
+//     `${baseURL}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`
+//   );
 
-  return {
-    props: {
-      properties: data?.hits,
-    },
-  };
-}
+//   return {
+//     props: {
+//       properties: data?.hits,
+//     },
+//   };
+// }
